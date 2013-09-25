9ac75dd6db9ea5f382da3dbc95dd4e24d1ec62d2
Student <user@chromebox-001.(none)>
2013-09-24 17:36:12 -0700

    Added TODO

--- ex1.py
+++ ex1.py
@@ -9,6 +9,9 @@ answer = random.randint(1,100)
 guess = 0
 count = 1
 
+# TODO: use ord() to examine each character in the input, 
+# test if between and including 48-57 (0-9)
+
 while guess != answer:
     guess = int(raw_input("Your guess? "))
     if guess > answer:
